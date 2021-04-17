a309b28e9be257f6709871b3c59cf51aced301fd
Lautaro Amengual <amengual.ln@gmail.com>
2021-04-17 12:24:29 -0300

    Ejercicio 3 implementado. Lee el contenido de package.json, lo muestra como objeto y string, y escribe el contenido en info.txt

--- TP3/routes/index.js
+++ TP3/routes/index.js
@@ -2,6 +2,9 @@ import express from "express";
 
 const router = express.Router();
 
+import { promises } from "fs";
+
+// Muestra un saludo según la hora del día
 router.get("/", (req, res) => {
   const date = new Date();
   const hora = date.getHours();
@@ -14,6 +17,7 @@ router.get("/", (req, res) => {
   }
 });
 
+// Devuelve un objeto cuyas claves son numeros aleatorios del 1 al 20, y los valores son la cantidad de apariciones de esos numeros
 router.get("/random", (req, res) => {
   let numeros = {};
   for (let n = 0; n <= 10000; n++) {
@@ -26,8 +30,25 @@ router.get("/random", (req, res) => {
   res.send(numeros);
 });
 
+// Mediante promesas, lee el package.json del proyecto, muestra su contenido como objeto y como string, y escribe el contenido en info.txt
+router.get("/info", async (req, res) => {
+  const file = "package.json";
+  const stats = await promises.stat(file);
+  const data = await promises.readFile(file);
+  const info = {
+    contenidoStr: JSON.stringify(JSON.parse(data)),
+    contenidoObj: JSON.parse(data),
+    size: stats["size"],
+  };
+
+  await promises.writeFile("info.txt", JSON.stringify(info, null, 2));
+
+  console.log(info);
+  res.send("Información del proyecto guardad en info.txt");
+});
+
 router.all("*", (req, res) => {
   res.status(404).send("404");
 });
 
-module.exports = router;
+export default router;
