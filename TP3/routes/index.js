333d599308dc78da7a0ece79df07f3e9212337b8
Lautaro Amengual <amengual.ln@gmail.com>
2021-04-17 13:03:00 -0300

    Muevo lógica a un controller

--- TP3/routes/index.js
+++ TP3/routes/index.js
@@ -3,50 +3,24 @@ import express from "express";
 const router = express.Router();
 
 import { promises } from "fs";
+import { saludo, random, info } from "../controller.js";
 
-// Muestra un saludo según la hora del día
+// Ejercicio 1
 router.get("/", (req, res) => {
-  const date = new Date();
-  const hora = date.getHours();
-  if (hora >= 6 && hora <= 12) {
-    res.send("Buenos dias!");
-  } else if (hora >= 13 && hora <= 19) {
-    res.send("Buenas tardes!");
-  } else {
-    res.send("Buenas noches!");
-  }
+  res.send(saludo());
 });
 
-// Devuelve un objeto cuyas claves son numeros aleatorios del 1 al 20, y los valores son la cantidad de apariciones de esos numeros
+// Ejercicio 2
 router.get("/random", (req, res) => {
-  let numeros = {};
-  for (let n = 0; n <= 10000; n++) {
-    let random = Math.floor(Math.random() * 20);
-    if (!(random in numeros)) {
-      numeros[random] = 0;
-    }
-    numeros[random]++;
-  }
-  res.send(numeros);
+  res.send(random());
 });
 
-// Mediante promesas, lee el package.json del proyecto, muestra su contenido como objeto y como string, y escribe el contenido en info.txt
+//  Ejercicio 3
 router.get("/info", async (req, res) => {
-  const file = "package.json";
-  const stats = await promises.stat(file);
-  const data = await promises.readFile(file);
-  const info = {
-    contenidoStr: JSON.stringify(JSON.parse(data)),
-    contenidoObj: JSON.parse(data),
-    size: stats["size"],
-  };
-
-  await promises.writeFile("info.txt", JSON.stringify(info, null, 2));
-
-  console.log(info);
-  res.send("Información del proyecto guardad en info.txt");
+  res.send(await info());
 });
 
+// 404 para las rutas que no esten contempladas
 router.all("*", (req, res) => {
   res.status(404).send("404");
 });
