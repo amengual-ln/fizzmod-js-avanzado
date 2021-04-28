632c8f3538e00cf0e6d4f47968c5faa7d951e329
Lautaro Amengual <amengual.ln@gmail.com>
2021-04-27 23:36:47 -0300

    Conexión con Mongo

--- TP4/index.js
+++ TP4/index.js
@@ -1,14 +1,24 @@
 import express from "express";
 import router from "./src/router/index.js";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set('views', './src/views')
-app.set('view engine', 'pug')
+app.set("views", "./src/views");
+app.set("view engine", "pug");
 
-app.use(express.static('public'))
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
 app.use("/", router);
+
+mongoose.connect(
+  "mongodb+srv://r42:qsmtlqnsn@cluster0.bxyib.mongodb.net/fizzmod-tp4?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.listen(3000);
 console.log(`Servidor escuchando en el puerto ${PORT}`);
